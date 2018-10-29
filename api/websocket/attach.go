238730a64b3163afc0558324fe1af9e47c88060e
Oleksandr Andriienko <oandriie@redhat.com>
2018-10-29 11:58:56 +0200

    Fix compilation.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- api/websocket/attach.go
+++ api/websocket/attach.go
@@ -46,7 +46,6 @@ func Attach(w http.ResponseWriter, r *http.Request, restParmas rest.Params) erro
 	if err = exec.GetExecManager().Attach(id, wsConn); err != nil {
 		log.Println("Attach connection is refused with id: ", id, err.Error())
 		log.Println("Try replace this exec by another one for test purpose ...")
-		exec.CreateExecManager().Create(exec.GetExecManager().GetById(id))
 		return err
 	}
 
